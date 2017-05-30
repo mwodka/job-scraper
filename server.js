621a19f7336496928a604b306bc8d6ab369873a7
Mitchel Wodka <mrw.simple@gmail.com>
2017-05-30 14:38:20 -0500

    changed port for server to process.env.PORT || 3000 for heroku support

--- server.js
+++ server.js
@@ -158,6 +158,10 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(process.env.PORT || 3000, function() {
+  if (process.env.PORT == undefined) {
+    console.log("App running on port 3000!");
+  } else {
+    console.log("App running on port " + process.env.PORT);
+  }
 });
